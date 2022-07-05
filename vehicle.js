9db5fa1ffd92d1df14218aae7269c1e0462d723d
eansengchang <76442284+eansengchang@users.noreply.github.com>
2022-07-05 16:26:55 +0100

    Revert "first commit"
    This reverts commit 4abdd94df18b8fd840a2cd23623aafb3477b3ab3.

--- vehicle.js
+++ vehicle.js
@@ -1,75 +1,77 @@
-function Vehicle(x, y) {
-  this.pos = createVector(x, y); //createVector(random(width), random(height));
-  this.vel = p5.Vector.random2D();
-  this.acc = createVector();
-  this.target = createVector(x, y);
-  this.r = 4;
-  this.maxSpeed = random(7, 12);
-  this.maxForce = random(0.3, 0.5);
-  this.slowRadius = 100;
-  this.runRadius = random(70, 90);
-
-  this.active = true;
+function Vehicle(x, y, text){
+    this.pos = createVector(random(width), random(height));
+    this.vel = p5.Vector.random2D();
+    this.acc = createVector();
+    this.target = createVector(x, y);
+    this.r = 2
+    this.maxSpeed = random(7, 12);
+    this.maxForce = random(0.3, 0.5);
+    this.slowRadius = 100;
+    this.runRadius = random(70, 90);
+    this.text = text
 }
 
-Vehicle.prototype.applyBehaviors = function () {
-  let arrive = this.arrive(this.target);
-  let mouse = createVector(mouseX, mouseY);
-  let flee = this.flee(mouse);
+Vehicle.prototype.applyBehaviors = function(){
+    let arrive = this.arrive(this.target);
+    let mouse = createVector(mouseX, mouseY);
+    let flee = this.flee(mouse);
 
-  flee.mult(3);
+    flee.mult(3)
 
-  this.applyForce(arrive);
-  this.applyForce(flee);
-};
+    this.applyForce(arrive)
+    this.applyForce(flee)
+}
 
-Vehicle.prototype.applyForce = function (force) {
-  this.acc.add(force);
-};
+Vehicle.prototype.applyForce = function(force){
+    this.acc.add(force)
+}
 
-Vehicle.prototype.update = function () {
-  if (!this.active) return;
-  this.applyBehaviors();
-  this.pos.add(this.vel);
-  this.vel.add(this.acc);
-  this.acc.mult(0);
-};
+Vehicle.prototype.update = function(){
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc.mult(0)
+}
 
-Vehicle.prototype.show = function () {
-    if (!this.active) return;
-  stroke(255);
+Vehicle.prototype.show = function(){
+	colorMode(HSB)
+    
+    stroke(255)
 
-  strokeWeight(this.r);
-  point(this.pos.x, this.pos.y);
-};
+    if(this.text.toLowerCase() == "rainbow" || this.text.toLowerCase() == "reyaansh patil"){
+        stroke(map(this.target.x, width / 2 - textWidth(TEXT) / 2, width / 2 + textWidth(TEXT) / 2, 0, 255), 255, 255)
+    }
 
-Vehicle.prototype.arrive = function (target) {
-  let desiredForce = p5.Vector.sub(target, this.pos);
-  let desiredSpeed = this.maxSpeed;
-  let distance = desiredForce.mag();
-  if (distance < this.slowRadius) {
-    desiredSpeed = map(distance, 0, this.slowRadius, 1, this.maxSpeed);
-  }
+    strokeWeight(this.r)
+    point(this.pos.x, this.pos.y)
+}
 
-  desiredForce.setMag(desiredSpeed);
-  let steer = p5.Vector.sub(desiredForce, this.vel);
-  steer.limit(this.maxForce);
-  return steer;
-};
+Vehicle.prototype.arrive = function(target){
+    let desiredForce = p5.Vector.sub(target, this.pos);
+    let desiredSpeed = this.maxSpeed
+    let distance = desiredForce.mag();
+    if(distance < this.slowRadius){
+        desiredSpeed = map(distance, 0, this.slowRadius, 1, this.maxSpeed)
+    }
 
-Vehicle.prototype.flee = function (target) {
-  let desiredForce = p5.Vector.sub(target, this.pos);
-  let distance = desiredForce.mag();
-  let desiredSpeed = 0;
-
-  if (distance < this.runRadius) {
-    desiredForce.setMag(this.maxSpeed);
-    desiredForce.mult(-1);
+    desiredForce.setMag(desiredSpeed);
     let steer = p5.Vector.sub(desiredForce, this.vel);
-    desiredSpeed = map(distance, 0, this.slowRadius, this.maxSpeed, 0);
-    steer.limit(this.maxForce);
-    return steer;
-  }
+    steer.limit(this.maxForce)
+    return steer
+}
 
-  return createVector(0, 0);
-};
+Vehicle.prototype.flee = function(target){
+    let desiredForce = p5.Vector.sub(target, this.pos);
+    let distance = desiredForce.mag();
+    let desiredSpeed = 0
+
+    if(distance < this.runRadius){
+        desiredForce.setMag(this.maxSpeed);
+        desiredForce.mult(-1)
+        let steer = p5.Vector.sub(desiredForce, this.vel);
+        desiredSpeed = map(distance, 0, this.slowRadius, this.maxSpeed,0)
+        steer.limit(this.maxForce)
+        return steer
+    }
+
+    return createVector(0, 0)
+}
